981d5df4a4ae1fb2de21385e1676250c5a5c5c38
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-09-10 16:41:41 +0200

    start controller, routes, view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :social_media, :strategie_social_media]
 
   def home
   end
+
+  def social_media
+  end
+
+  def strategie_social_media
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'social_media',            to: 'pages#social_media',      as: :social_media
+  get 'strategie_social_media', to: 'pages#strategie_social_media', as: :strategie_social_media
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
